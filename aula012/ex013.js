e99293e914ba79a192f702911e155afdfb697a16
Kelvin Santos <kelvin_re@hotmail.com>
2021-11-03 02:07:44 -0300

    Update ex013.js

--- aula012/ex013.js
+++ aula012/ex013.js
@@ -1,1 +1,15 @@
-var  hora = 8
+var hora_atual = new Date
+var hora = hora_atual.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if(hora < 6){
+    console.log('Boa Madrugada!')
+}
+else if(hora <= 12){
+    console.log('Bom dia!')
+}
+else if(hora <=18){
+    console.log('Boa Tarde!')
+}
+else{
+    console.log('Boa Noite')
+}
