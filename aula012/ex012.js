a994cf8133ab0f1be4e8e964a81184f136bf1506
Kelvin Santos <kelvin_re@hotmail.com>
2021-11-03 01:49:12 -0300

    Atualização dos exercícios

---
+++ aula012/ex012.js
@@ -0,0 +1,11 @@
+var idade = 15
+console.log(`Você tem ${idade} anos.`)
+if(idade < 16){
+    console.log('Não vota')
+}
+else if(idade < 18){
+    console.log('voto opcional')
+}
+else{
+    console.log('voto obrigatório')
+}
