5047ae29e33d7c34504e0b845763cfa7eed9642b
Kelvin Santos <kelvin_re@hotmail.com>
2021-11-06 15:19:55 -0300

    Atualização dos exercícios

---
+++ aula014(parte 2)/ambiente.js
@@ -0,0 +1,21 @@
+// Lanços de Repetições
+
+
+// while(c <= 10){
+//     console.log(`passo ${c}`)
+//     c++
+// }
+
+
+// function teste(c) {
+//     do {
+//         console.log(`passo ${c}`)
+//         c++
+//     } while (c <= 15)
+// }
+// teste(10)
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`passo ${i}`);
+}
