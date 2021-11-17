f699e71ad3569d0d75b20defbccc44d6254c5b73
Kelvin Santos <kelvin_re@hotmail.com>
2021-11-17 18:19:59 -0300

    Atualização da função

--- aula016/funcao01.js
+++ aula016/funcao01.js
@@ -1,11 +1,11 @@
 function parimp(n) {
     if (n % 2 == 0) {
-        return `Número ${n} PAR!`
+        return `Número ${n} é PAR!`
     }
     else{
-        return `Número ${n} ÍMPAR!`
+        return `Número ${n} é ÍMPAR!`
     }
 }
 
-let res = parimp(5)
-console.log(res)
+// let res = parimp(3)
+console.log(parimp(3)) // Eu tbm posso fazer assim!
